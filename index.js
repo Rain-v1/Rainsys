2fa420c7a190df0fa98a3a3aea9707e588ecf382
Rain-v1 <rainy7daysa@gmail.com>
2025-01-25 19:14:14 -0800

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
+const statusMessages = ["Assisting in Studio"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
